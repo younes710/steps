bc4a91e8fc3a4e997becc2e5b19d87d2c5f4312a
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-12 14:33:11 +0330

    Add App component styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-function App() {
-    return <div>index</div>;
-}
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
