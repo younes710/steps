774c652a6502dd5bdee067bcb4e61831544b21fc
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-16 13:18:38 +0330

    Create Button component and finish project

--- src/App.js
+++ src/App.js
@@ -38,29 +38,39 @@ function App() {
                         Step {step}: {messages[step - 1]}
                     </p>
                     <div className='buttons'>
-                        <button
+                        <Button
+                            bgColor='#7950f2'
+                            textColor='#fff'
                             onClick={handlePrevious}
-                            style={{
-                                backgroundColor: '#7950f2',
-                                color: '#fff',
-                            }}
                         >
-                            Previous
-                        </button>
-                        <button
+                            <span>👈</span> Previous
+                        </Button>
+                        <Button
+                            bgColor='#7950f2'
+                            textColor='#fff'
                             onClick={handleNext}
-                            style={{
-                                backgroundColor: '#7950f2',
-                                color: '#fff',
-                            }}
                         >
-                            Next
-                        </button>
+                            Next <span>👉</span>
+                        </Button>
                     </div>
                 </div>
             )}
         </>
     );
 }
+
+const Button = ({ textColor, bgColor, onClick, children }) => {
+    return (
+        <button
+            style={{
+                backgroundColor: bgColor,
+                color: textColor,
+            }}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+};
 
 export default App;
